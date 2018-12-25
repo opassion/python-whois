94f496c7236794a7c00f74f430460c0469955abd
opassion <objcdeveloperx@gmail.com>
2018-12-25 12:24:39 +0300

    parse main domains incase it's subdomain

--- pythonwhois/__init__.py
+++ pythonwhois/__init__.py
@@ -1,6 +1,26 @@
 from . import net, parse
 
 def get_whois(domain, normalized=[]):
+	result = get_whois_main(domain, normalized)
+	# unless creation_date is not found, consider the given one is subdomain
+	# and check again with main domain
+	# i.e. mail.xxx.com -> xxx.com
+
+	subdomain = domain
+
+	while True:
+		if "creation_date" in result and result["creation_date"] != None:
+			break
+		parts = subdomain.split(".")
+		if len(parts) <= 2:
+			break
+		subdomain = ".".join(parts[1:])
+		result = get_whois_main(subdomain, normalized)
+		if "creation_date" in result and result["creation_date"] != None:
+			break
+	return result
+
+def get_whois_main(domain, normalized=[]):
 	raw_data, server_list = net.get_whois_raw(domain, with_server_list=True)
 	# Unlisted handles will be looked up on the last WHOIS server that was queried. This may be changed to also query
 	# other servers in the future, if it turns out that there are cases where the last WHOIS server in the chain doesn't
